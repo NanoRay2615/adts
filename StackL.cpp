71d3ee1ca35b6cb3dc1568edf049ba620a117aaf
NanoRay2615 <NanoRay2615@users.noreply.github.com>
2017-03-18 20:03:41 -0400

    Updated StackL,StackLL and StackV .cpp files

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,35 @@
 #include "StackL.h"
+using namespace std;
+
+int Stack::size()
+{
+ 	return data.size();
+
+}
+
+int Stack::top()
+{
+
+return data.get(1);
+
+}
+
+void Stack::pop()
+{
+
+	data.remove(1);
+}
+
+void Stack::push(int val)
+{
+	data.insert(val,1);
+}
+
+void Stack::clear()
+{
+
+	data.~List();
+}
+
+
+
--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,32 @@
 #include "StackV.h"
+#include <vector>
+using namespace std;
+
+
+int Stack::size()
+{
+	return data.size();
+}
+
+int Stack::top()
+{
+	return data[data.size()-1];
+}
+
+void Stack::push(int val)
+{
+	data.push_back(val);
+}
+
+void Stack::pop()
+{
+	data.pop_back();
+}
+
+void Stack::clear()
+{
+	for (int i=0; i<data.size(); ++i)
+	{
+		pop();
+	}
+}
